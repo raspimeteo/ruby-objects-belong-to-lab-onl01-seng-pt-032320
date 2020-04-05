c781013fe0cc90bbcec4a7cf36c4a90dba4495f0
John Zonneveld <jjczon@gmail.com>
2020-04-05 23:11:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,8 @@
 class Song
- attr_accessor :title
-	 
-	  def initialize(title)
-	    @title = title
-	    @artist = artist
-	  end
-	end
+attr_accessor :title, :artist
 
+    def initialize(title)
+        @title = title
+    end
+
+end
